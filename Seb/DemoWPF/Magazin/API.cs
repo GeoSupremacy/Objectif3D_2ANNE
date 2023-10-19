b026d912804c6a8e7dff29880e5984eaefb9132a
GUICHARD Benoit <100837994+GeoSupremacy@users.noreply.github.com>
2023-10-19 10:01:09 +0200

    Add MAPi in Magasin

--- Seb/DemoWPF/Magazin/API.cs
+++ Seb/DemoWPF/Magazin/API.cs
@@ -14,7 +14,11 @@ namespace Magazin
         public static string GetRandomQuote => $"{BASE_URL}/random";
 
         public static string GetListOfGames(string _title) => $"{BASE_URL}games?title={_title}";
-        public static string GETMultipleGameLookup(string _character = "tyrion", uint _number = 1) => $"{BASE_URL}/author/{_character}/{_number}";
+        public static string GetMultipleGameLookup(string _character = "tyrion", uint _number = 1) => $"{BASE_URL}games?title=";
 
+        //_storeID sert à filtrer les magasins qui a le jeu si non définit affiche tous les magasins
+        //upperPrice n'affiche que les prix en dessous ou égale au montant désigner (50 équivaut à aucune limite)
+        //D'autre paramètre qui sont optionnel
+        public static string GetListOfDeals(string _storeID, string _upperPrice) => $"{BASE_URL}deals?storeID={_storeID}&upperPrice={_upperPrice}";
     }
 }
